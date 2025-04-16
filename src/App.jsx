0eef5e153f32ec4f649bc310a7a329e1f2bc6fe0
EmanAwsIsmail <eman.aws.ismail@gmail.com>
2025-04-16 21:35:13 +0400

    set

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import Profile from "./components/Profile";
 import Courses from "./components/Courses";
 import Missions from "./components/Missions";
 import Shop from "./components/Shop";
+import Bookmarks from "./components/Bookmarks"; //new
 import Settings from "./components/Settings";
 import Page from "./components/Page";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
+import SubUnit from "./components/Subunit"; //new
 import Questions from "./components/Questions"; 
 
 function App() {
@@ -19,9 +21,12 @@ function App() {
                 <Route path="/signup" element={<Signup />} />
                 <Route path="/dashboard" element={<Page><Dashboard /></Page>} />
                 <Route path="/profile" element={<Page><Profile /></Page>} />
-                <Route path="/courses" element={<Page><Courses /></Page>} />
+                <Route path="/courses" element={<Page><Courses /></Page>}>
+                    <Route path="subunit/:subUnitId" element={<SubUnit />} /> 
+                </Route>
                 <Route path="/missions" element={<Page><Missions /></Page>} />
                 <Route path="/shop" element={<Page><Shop /></Page>} />
+                <Route path="/bookmarks" element={<Page><Bookmarks /></Page>} />
                 <Route path="/settings" element={<Page><Settings /></Page>} />
                 <Route path="/questions/:unitId/:subunitId" element={<Page><Questions /></Page>} />
             </Routes>
